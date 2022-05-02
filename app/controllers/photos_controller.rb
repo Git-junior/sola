7bc875ebedb5b71e4452551d27506c4ff151f0d3
Git-junior <juni0r.kiza6@gmail.com>
2022-05-02 17:56:15 +0900

    投稿機能の実装

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,9 +4,22 @@ class PhotosController < ApplicationController
   end
 
   def new
+    @photo = Photo.new
   end
 
   def create
+    @photo = Photo.new(photo_params)
+    if @photo.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:content, :image).merge(user_id: current_user.id)
   end
   
 end

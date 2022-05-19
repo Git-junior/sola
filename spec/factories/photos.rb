5c9cab5cd263a6faff4b4906c561f2387277de6e
Git-junior <juni0r.kiza6@gmail.com>
2022-05-19 23:28:39 +0900

    プロトタイプ情報をFactoryBotで指定

--- spec/factories/photos.rb
+++ spec/factories/photos.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :photo do
-    
+    content {Faker::Lorem.sentence}
+    association :user
+
+    after(:build) do |photo|
+      photo.image.attach(io: File.open('public/images/test_image.JPG'), filename: 'test_image.JPG')
+    end
   end
 end

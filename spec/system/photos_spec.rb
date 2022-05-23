bc2468c4e6973be053bf5506d1a768f7a2a03526
Git-junior <juni0r.kiza6@gmail.com>
2022-05-23 18:30:34 +0900

    プロトタイプ情報投稿結合テストコード

--- spec/system/photos_spec.rb
+++ spec/system/photos_spec.rb
@@ -15,50 +15,90 @@ RSpec.describe 'プロトタイプ情報投稿', type: :system do
   context 'ログインしていないとき' do
     it 'ログインしていない状態で新規投稿ページにアクセスした場合、ログインページに移動する' do
       # トップページに移動する
+      basic_pass root_path
+      visit new_photo_path
       # ログインしていない場合、ログインページに遷移されることを確認する
+      expect(current_path).to eq(new_user_session_path)
     end
   end
   context 'プロトタイプ情報投稿できるとき' do
     it 'ログイン状態で、必要な情報が存在していれば新規投稿できる' do
       # ログインする
+      sign_in(@user)
       # 新規投稿ページへのボタンがあることを確認する
+      expect(page).to have_content('新規投稿')
       # 新規投稿ページに移動する
+      visit new_photo_path
       # フォームに情報を入力する
+      fill_in 'photo[content]', with: @photo_content
       # 添付する画像を定義する
+      image_path = Rails.root.join('public/images/test_image.JPG')
       # 画像選択フォームに画像を添付する
+      attach_file('photo[image]', image_path)
       # 保存するボタンを押すとPhotoモデルのカウントが1上がることを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { Photo.count }.by(1)
       # トップページには先ほど投稿した内容の画像情報が存在することを確認する
+      expect(page).to have_selector('img')
       # トップページには先ほど投稿した内容の文字情報が存在することを確認する
+      expect(page).to have_content(@photo_content)
     end
   end
   context 'プロトタイプ情報投稿できないとき' do
     it 'ログイン状態だが、テキストが空では新規投稿ができずに同じページに戻ってくる' do
       # ログインする
+      sign_in(@user)
       # 新規投稿ページへのボタンがあることを確認する
+      expect(page).to have_content('新規投稿')
       # 新規投稿ページに移動する
+      visit new_photo_path
       # フォームに情報を入力しない
+      fill_in 'photo[content]', with: ''
       # 添付する画像を定義する
+      image_path = Rails.root.join('public/images/test_image.JPG')
       # 画像選択フォームに画像を添付する
+      attach_file('photo[image]', image_path)
       # 保存するボタンを押してもPhotoモデルのカウントは上がらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { Photo.count }.by(0)
       # 新規投稿ページへ戻されることを確認する
+      expect(current_path).to eq(photos_path)
     end
     it 'ログイン状態だが、画像が空では新規投稿ができずに同じページに戻ってくる' do
       # ログインする
+      sign_in(@user)
       # 新規投稿ページへのボタンがあることを確認する
+      expect(page).to have_content('新規投稿')
       # 新規投稿ページに移動する
+      visit new_photo_path
       # フォームに情報を入力する
+      fill_in 'photo[content]', with: @photo_content
       # 画像を添付しない
       # 保存するボタンを押してもPhotoモデルのカウントは上がらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { Photo.count }.by(0)
       # 新規投稿ページへ戻されることを確認する
+      expect(current_path).to eq(photos_path)
     end
     it 'ログイン状態だが、テキストと画像が空では新規投稿ができずに同じページに戻ってくる' do
       # ログインする
+      sign_in(@user)
       # 新規投稿ページへのボタンがあることを確認する
+      expect(page).to have_content('新規投稿')
       # 新規投稿ページに移動する
+      visit new_photo_path
       # フォームに情報を入力しない
+      fill_in 'photo[content]', with: ''
       # 画像を添付しない
       # 保存するボタンを押してもPhotoモデルのカウントは上がらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { Photo.count }.by(0)
       # 新規投稿ページへ戻されることを確認する
+      expect(current_path).to eq(photos_path)
     end
   end
 end

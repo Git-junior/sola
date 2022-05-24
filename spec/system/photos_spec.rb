65c150d78ebb80b61cf33b374ef80a4674140ee1
Git-junior <juni0r.kiza6@gmail.com>
2022-05-24 17:19:51 +0900

    exampleの編集

--- spec/system/photos_spec.rb
+++ spec/system/photos_spec.rb
@@ -16,8 +16,9 @@ RSpec.describe 'プロトタイプ情報投稿', type: :system do
     it 'ログインしていない状態で新規投稿ページにアクセスした場合、ログインページに移動する' do
       # トップページに移動する
       basic_pass root_path
+      # 新規投稿ページにアクセスをする
       visit new_photo_path
-      # ログインしていない場合、ログインページに遷移されることを確認する
+      # ログインページに遷移されることを確認する
       expect(current_path).to eq(new_user_session_path)
     end
   end
@@ -39,6 +40,8 @@ RSpec.describe 'プロトタイプ情報投稿', type: :system do
       expect{
         find('input[name="commit"]').click
       }.to change { Photo.count }.by(1)
+      # トップページに遷移する
+      visit root_path
       # トップページには先ほど投稿した内容の画像情報が存在することを確認する
       expect(page).to have_selector('img')
       # トップページには先ほど投稿した内容の文字情報が存在することを確認する
@@ -113,8 +116,9 @@ RSpec.describe 'プロトタイプ情報詳細表示', type: :system do
     it 'ログインしていない状態で詳細表示ページにアクセスした場合、ログインページに移動する' do
       # トップページに移動する
       basic_pass root_path
+      # 詳細表示ページにアクセスする
       visit photo_path(@photo1)
-      # ログインしていない場合、ログインページに遷移されることを確認する
+      # ログインページに遷移されることを確認する
       expect(current_path).to eq(new_user_session_path)
     end
   end
@@ -133,7 +137,7 @@ RSpec.describe 'プロトタイプ情報詳細表示', type: :system do
     it 'ログイン状態だが、自分以外が投稿したプロトタイプ情報は詳細表示できずトップページに移動する' do
       # ログインする
       sign_in(@photo1.user)
-      # 自分以外が投稿したプロトタイプ情報の詳細ページに遷移する
+      # 自分以外が投稿したプロトタイプ情報の詳細ページにアクセスする
       visit photo_path(@photo2)
       # 自分のトップページに遷移されることを確認する
       expect(current_path).to eq(photos_index_path)
@@ -151,8 +155,9 @@ RSpec.describe 'プロトタイプ情報編集', type: :system do
     it 'ログインしていない状態で編集ページにアクセスした場合、ログインページに移動する' do
       # トップページに移動する
       basic_pass root_path
+      # 編集ページにアクセスする
       visit edit_photo_path(@photo1)
-      # ログインしていない場合、ログインページに遷移されることを確認する
+      # ログインページに遷移されることを確認する
       expect(current_path).to eq(new_user_session_path)
     end
   end
@@ -192,7 +197,7 @@ RSpec.describe 'プロトタイプ情報編集', type: :system do
     it 'ログイン状態だが、自分以外が投稿したプロトタイプ情報は編集できずトップページに移動する' do
       # ログインする
       sign_in(@photo1.user)
-      # 自分以外が投稿したプロトタイプ情報の編集ページに遷移する
+      # 自分以外が投稿したプロトタイプ情報の編集ページにアクセスする
       visit edit_photo_path(@photo2)
       # 自分のトップページに遷移されることを確認する
       expect(current_path).to eq(photos_index_path)
@@ -210,8 +215,9 @@ RSpec.describe 'プロトタイプ情報削除', type: :system do
     it 'ログインしていない状態で削除ボタンのある詳細表示ページにアクセスした場合、ログインページに移動する' do
       # トップページに移動する
       basic_pass root_path
+      # 詳細表示ページにアクセスする
       visit photo_path(@photo1)
-      # ログインしていない場合、ログインページに遷移されることを確認する
+      # ログインページに遷移されることを確認する
       expect(current_path).to eq(new_user_session_path)
     end
   end
@@ -239,7 +245,7 @@ RSpec.describe 'プロトタイプ情報削除', type: :system do
     it 'ログイン状態だが、自分以外が投稿したプロトタイプ情報は削除できずトップページに移動する' do
       # ログインする
       sign_in(@photo1.user)
-      # 自分以外が投稿したプロトタイプ情報の編集ページに遷移する
+      # 自分以外が投稿したプロトタイプ情報の詳細表示ページにアクセスする
       visit photo_path(@photo2)
       # 自分のトップページに遷移されることを確認する
       expect(current_path).to eq(photos_index_path)
